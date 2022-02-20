8dd35a137153d33b345c806411086b18937b3873
zhuqingguang03 <zhuqingguang03@kuaishou.com>
2022-02-20 21:27:59 +0800

    feat: finish git push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gitpush
 ---------------
+
+run `git push` and automatically run `git push --set-upstream origin branch-name` when there is no upstream.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
-use std::fs::read_to_string;
+use std::{fs::read_to_string, process::ExitStatus};
 
 use clap::Parser;
 use anyhow::{Context, Result};
 use log::{info, warn};
+use std::process::Command;
+use regex::Regex;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,29 +15,45 @@ struct Cli {
     #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
+
 fn main() -> Result<()> {
-    let pattern = std::env::args().nth(1).expect("no pattern giver");
-    let path = std::env::args().nth(2).expect("no path giver");
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
+    let args: Vec<String> = std::env::args().collect();
+
+    let output = Command::new("git").arg("push").args(&args[1..]).output().expect("failed to execute \"git push\"");
+
+    if output.status.success() {
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+        return Ok(());
+    }
+
+    let output_string = String::from_utf8_lossy(&output.stderr);
+    if !output_string.contains("To push the current branch and set the remote as upstream, use") {
+        println!("{}", &output_string);
+        return Ok(());
+    }
+
+    let re = Regex::new(r"git push[ \-\w]+\n").unwrap();
+    let cmd_to_run = match re.find(&output_string) {
+        Some(x) => &output_string[(x.start() + 4)..(x.end() - 1)],
+        _ => "",
     };
-    let args = Cli::parse();
+    println!("cmd_to_run is {}", cmd_to_run);
+    let args: Vec<&str> = cmd_to_run.split(" ")
+        .filter(|x| *x != "git").collect();
+    let output =  Command::new("git")
+        .args(
+            args
+        )
+        .output()
+        .expect(&(String::from("failed to execute \" git ") + cmd_to_run + &String::from("\"")));
 
-    println!("Your pattern is {}, and your path is {:?}", args.pattern, args.path);
-
-    // let content = std::fs::read_to_string(&args.path)
-    //     .expect("could not read file");
-
-    let content = read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{:?}`", args.path))?;
-
-    gitpush::find_matches(&content, &args.pattern, &mut std::io::stdout());
-
-    // env_logger::init();
-    // info!("starting up");
-    // warn!("oops, nothing implemented!");
-
+    let str = String::from_utf8_lossy(
+        match &output.status.success() {
+            true => &output.stdout,
+            false => &output.stderr,
+        }
+    );
+    println!("{}", str);
     Ok(())
 
 }
